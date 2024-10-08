cf7aefa37b84d5c1459076b52e58786bdd0f81eb
David Eliason <davethemaker@gmail.com>
2024-10-08 11:26:33 -0700

    add login function and imporrt to main app.py file and invoke that function when appropriate menu choice is selected

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from steps_pkg.homepage import show_homepage
+from steps_pkg.user import login
+
 # show_homepage() # test
 database = {"admin":"password123"}
 quotes = []
@@ -18,6 +20,9 @@ if option == '1':
     # login functionality
     username = input('Enter username: ')
     password = input('Enter password: ')
+
+    authorized_user = login(database,username,password)
+
 
 elif option == '2':
 	# register functionality
