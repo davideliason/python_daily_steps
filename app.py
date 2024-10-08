bfd14156554b79d925a54b02042515979921b23b
David Eliason <davethemaker@gmail.com>
2024-10-08 11:02:55 -0700

    check to see if user is logged in or not

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ from steps_pkg.homepage import show_homepage
 database = {"admin":"password123"}
 quotes = []
 authorized_user = ""
+
+while True:
+	show_homepage()
+
+	if authorized_user == "":
+		print("You have to be logged in first.")
+	else:
+		print("Logged in as: ", authorized_user)
