9098524d676fff05721b7098051dc6f9ba23e3e9
David Eliason <davethemaker@gmail.com>
2024-10-08 11:11:24 -0700

    add placeholder functionality for other input selections

--- app.py
+++ app.py
@@ -15,6 +15,29 @@ else:
 option = input("Enter an option:")
 
 if option == '1':
-        # login functionality
-        username = input('Enter username: ')
-        password = input('Enter password: ')
+    # login functionality
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+
+elif option == '2':
+	# register functionality
+	print("make register fx")
+	show_homepage()
+
+elif option == '3':
+	# make quote functionality
+	print("make quote fx")
+	show_homepage()
+
+elif option == '4':
+	# list quotes fx
+	print("make list quotes fx")
+	show_homepage()
+
+elif option == '5':
+	# say goodbye and exit
+	print("Goodbye")
+	exit()
+
+
+
