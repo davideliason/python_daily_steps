ef26cbf13eb6f3f68b0c3a710eafbe1d9777fe86
David Eliason <davethemaker@gmail.com>
2024-10-12 20:04:56 -0700

    refactor so that the search function correctly finds the weight value for an authorized user if a specified date is given

--- app.py
+++ app.py
@@ -95,12 +95,10 @@ while True:
 
 			for data in loaded_data:
 				for key in data:
-					print(f"this is the key: {key}")
 					if key == 'user' and data[key] == username:
-						print("found key")
+						print(f'Weight: {data["weight"]}')
 						next
-						if key == 'date':
-							print("this is the date")
+						
 					else:
-						print("no data")
+						continue
 						exit
